6d3e3846c60e1dd2966f135205bdbdd582d03766
Erkieman <micahjmeier@gmail.com>
2023-10-05 22:33:30 -0500

    add logout to dashboard

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,11 +1,20 @@
 //create basic react component view for a dashboard that just says "you are logged in" 
-
+import Button from 'react-bootstrap/Button';
 import React from 'react';
 
-const Dashboard = () => {
+const Dashboard = (data) => {
+
+  const LogOut = () => {
+    /* global google */
+    google.accounts.id.disableAutoSelect();
+    console.log('Logged out');
+    console.log(data)
+  };
+
   return (
     <div>
       <h1>You are logged in</h1>
+      <Button onClick={LogOut}>Sign Out</Button>
     </div>
   );
 }
