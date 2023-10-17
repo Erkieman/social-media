ddb21230d99b36ce6d33e58035a10ff52f4ec163
Erkieman <micahjmeier@gmail.com>
2023-10-16 19:37:33 -0500

    add BrowserRouter from react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -9,11 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 root.render(
+  <BrowserRouter>
   <GoogleOAuthProvider clientId={clientId}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </GoogleOAuthProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
